667c4bba16ef654e561384b415a12f31196cd434
yaroslav <yaroslav.marchuk01@gmail.com>
2021-11-07 20:00:55 +0100

    Show some examples of image similarity

--- image_similarity_clothing.py
+++ image_similarity_clothing.py
@@ -5,5 +5,12 @@ path_images = './data/clothing-dataset-small-master/train'
 
 sim_clothing = ImageSimilarity(path_images)
 sim_clothing.create_similarity_model()
-sim_clothing.explore_similarities(1)
 
+similarities_100 = sim_clothing.find_similarity_for_id(100)
+similarities_100.explore()
+
+similarities_600 = sim_clothing.find_similarity_for_id(600)
+similarities_600.explore()
+
+similarities_2500 = sim_clothing.find_similarity_for_id(2500)
+similarities_2500.explore()
--- images_similarity/image_similarity.py
+++ images_similarity/image_similarity.py
@@ -14,8 +14,5 @@ class ImageSimilarity:
         query_results = self.model.query(self.reference_data.filter_by(image_id, 'id'), k=num_similar_images)
         similar_rows = query_results['reference_label']
         similar_images = self.reference_data.filter_by(similar_rows, 'id')
-        self.similar_images_with_score = similar_images.join(query_results, how='inner', on={'id':'reference_label'})
-        return self.similar_images_with_score
-
-    def explore_similarities(self):
-        self.similar_images_with_score.sort('distance', ascending=True).explore()
+        similar_images_with_score = similar_images.join(query_results, how='inner', on={'id':'reference_label'})
+        return similar_images_with_score.sort('distance', ascending=True)
